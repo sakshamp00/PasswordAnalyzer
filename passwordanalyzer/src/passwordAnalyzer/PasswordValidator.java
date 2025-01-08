572c7f65407463aad2d15b7993f38ecced259c15
Sheikh Bidito Haque <skbidito07@gmail.com>
2025-01-09 00:29:43 +0600

    Update PasswordValidator.java

--- passwordanalyzer/src/passwordAnalyzer/PasswordValidator.java
+++ passwordanalyzer/src/passwordAnalyzer/PasswordValidator.java
@@ -4,18 +4,31 @@ public class PasswordValidator {
     private passwordStrengthChecker strengthChecker;
 
 
-public PasswordValidator(String correctPassword, int minimumStrengthScore){
-    this.correctPassword = correctPassword;
-    this.minimumStrengthScore = minimumStrengthScore;
-    this.strengthChecker = new passwordStrengthChecker();
-}
+    public PasswordValidator(String correctPassword, int minimumStrengthScore){
+        this.correctPassword = correctPassword;
+        this.minimumStrengthScore = minimumStrengthScore;
+        this.strengthChecker = new passwordStrengthChecker();
+    }
 
-public boolean isPasswordValidat(String inputPassword) {
-    strengthChecker.checkPassword(inputPassword);
-    int strengthScore =strengthChecker.getScore();
-    boolean isStrongEnough = strengthScore >= minimumStrengthScore;
-    return isStrongEnough && inputPassword.equals(correctPassword);
-}
+    public boolean isPasswordValid(String inputPassword) {
+        strengthChecker.checkPassword(inputPassword);
+        int strengthScore =strengthChecker.getScore();
+        boolean isStrongEnough = strengthScore >= minimumStrengthScore;
+        return isStrongEnough && inputPassword.equals(correctPassword);
+    }
+
+    public String getPasswordStrengthLevel(String inputPassword) {
+        strengthChecker.checkPassword(inputPassword);
+        int strengthScore = strengthChecker.getScore();
+
+        if (strengthScore >= 50) {
+            return "High";
+        } else if (strengthScore >= 30) {
+            return "Medium";
+        } else {
+            return "Low";
+        }
+    }
 
 // For integration with a React frontend, we can add methods to expose validation results
     public String validateForFrontend(String inputPassword) {
